2c626bdb82373330cca524b53c20090e15bc899c
Lobosanplay <parra.santiago2008@gmail.com>
2026-05-02 18:10:30 -0500

    fix: better usage in services

--- features/documents/services/documents-service.ts
+++ features/documents/services/documents-service.ts
@@ -3,41 +3,45 @@ import { handleSupabaseQuery } from "@/core/lib/supabase-handler";
 import type { DocumentModel } from "../types";
 
 export function createDocummentService(document_data: DocumentModel) {
-  return handleSupabaseQuery(async () => {
-    return await supabaseClient
-      .from("documents_generated")
-      .insert(document_data)
-      .select("*");
-  });
+  return handleSupabaseQuery(
+    async () =>
+      await supabaseClient
+        .from("documents_generated")
+        .insert(document_data)
+        .select("*"),
+  );
 }
 
 export function getDocummentByAccountIdService(account_id: string) {
-  return handleSupabaseQuery(async () => {
-    return await supabaseClient
-      .from("documents_generated")
-      .select("*")
-      .eq("account_id", account_id);
-  });
+  return handleSupabaseQuery(
+    async () =>
+      await supabaseClient
+        .from("documents_generated")
+        .select("*")
+        .eq("account_id", account_id),
+  );
 }
 
 export function updateDocumentByIdService(
   document_data: DocumentModel,
   document_id: string,
 ) {
-  return handleSupabaseQuery(async () => {
-    return await supabaseClient
-      .from("documents_generated")
-      .update(document_data)
-      .eq("id", document_id)
-      .single();
-  });
+  return handleSupabaseQuery(
+    async () =>
+      await supabaseClient
+        .from("documents_generated")
+        .update(document_data)
+        .eq("id", document_id)
+        .single(),
+  );
 }
 
 export function markDeletedDocumentByIdService(document_id: string) {
-  return handleSupabaseQuery(async () => {
-    return await supabaseClient
-      .from("documents_generated")
-      .update({ deleted: true })
-      .eq("id", document_id);
-  });
+  return handleSupabaseQuery(
+    async () =>
+      await supabaseClient
+        .from("documents_generated")
+        .update({ deleted: true })
+        .eq("id", document_id),
+  );
 }
