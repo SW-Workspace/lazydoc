72206ba25474da01287256684783b6f82c9b58b7
RamCodeZ3 <arammusset7@gmail.com>
2026-05-03 15:05:06 -0400

    feat: se agrego el componente para manejar la edicion del markdown

---
+++ features/documents/utils/editor.ts
@@ -0,0 +1,17 @@
+import { EditorState } from "prosemirror-state";
+import { history } from "prosemirror-history";
+import { keymap } from "prosemirror-keymap";
+import { baseKeymap } from "prosemirror-commands";
+import { undo, redo } from "prosemirror-history";
+import { defaultMarkdownParser } from "prosemirror-markdown";
+
+export function createState(markdown = "") {
+  return EditorState.create({
+    doc: defaultMarkdownParser.parse(markdown),
+    plugins: [
+      history(),
+      keymap({ "Mod-z": undo, "Mod-y": redo }),
+      keymap(baseKeymap),
+    ],
+  });
+}
